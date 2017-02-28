1cf0184a2c280916a4a3764401c569a491d5756d
liamka <liamka@me.com>
2017-02-28 15:55:01 +0300

    1.12.28

--- volta.js
+++ volta.js
@@ -1,7 +1,7 @@
 window.Conf = {
     options: {
         render: {
-            attr: "volta",
+            attr: ["volta", "vt"],
             sub_strings: /^(align-content-(.*)|align-items-(.*)|align-self-(.*)|animation-(.*)|animation-delay-(.*)|animation-direction-(.*)|animation-duration-(.*)|animation-fill-mode-(.*)|animation-iteration-count-(.*)|animation-name-(.*)|animation-timing-function-(.*)|animation-play-state-(.*)|background-(.*)|background-attachment-(.*)|background-color-(.*)|background-image-(.*)|background-position-(.*)|background-repeat-(.*)|background-clip-(.*)|background-origin-(.*)|background-size-(.*)|backface-visibility-(.*)|border-(.*)|border-bottom-(.*)|border-bottom-color-(.*)|border-bottom-left-radius-(.*)|border-bottom-right-radius-(.*)|border-bottom-style-(.*)|border-bottom-width-(.*)|border-collapse-(.*)|border-color-(.*)|border-image-(.*)|border-image-outset-(.*)|border-image-repeat-(.*)|border-image-slice-(.*)|border-image-source-(.*)|border-image-width-(.*)|border-left-(.*)|border-left-color-(.*)|border-left-style-(.*)|border-left-width-(.*)|border-radius-(.*)|border-right-(.*)|border-right-color-(.*)|border-right-style-(.*)|border-right-width-(.*)|border-spacing-(.*)|border-style-(.*)|border-top-(.*)|border-top-color-(.*)|border-top-left-radius-(.*)|border-top-right-radius-(.*)|border-top-style-(.*)|border-top-width-(.*)|border-width-(.*)|bottom-(.*)|box-decoration-break-(.*)|box-shadow-(.*)|box-sizing-(.*)|caption-side-(.*)|clear-(.*)|clip-(.*)|color-(.*)|column-count-(.*)|column-fill-(.*)|column-gap-(.*)|column-rule-(.*)|column-rule-color-(.*)|column-rule-style-(.*)|column-rule-width-(.*)|columns-(.*)|column-span-(.*)|column-width-(.*)|content-(.*)|counter-increment-(.*)|counter-reset-(.*)|cursor-(.*)|direction-(.*)|display-(.*)|empty-cells-(.*)|filter-(.*)|flex-(.*)|flex-basis-(.*)|flex-direction-(.*)|flex-flow-(.*)|flex-grow-(.*)|flex-shrink-(.*)|flex-wrap-(.*)|float-(.*)+|css-float-(.*)|font-(.*)|font-family-(.*)|font-size-(.*)|font-style-(.*)|font-variant-(.*)|font-weight-(.*)|font-size-adjust-(.*)|font-stretch-(.*)|hanging-punctuation-(.*)|height-(.*)|hyphens-(.*)|icon-(.*)|image-orientation-(.*)|justify-content-(.*)|left-(.*)|letter-spacing-(.*)|line-height-(.*)|list-style-(.*)|list-style-image-(.*)|list-style-position-(.*)|list-style-type-(.*)|margin-(.*)|margin-bottom-(.*)|margin-left-(.*)|margin-right-(.*)|margin-top-(.*)|max-height-(.*)|max-width-(.*)|min-height-(.*)|min-width-(.*)|nav-down-(.*)|nav-index-(.*)|nav-left-(.*)|nav-right-(.*)|nav-up-(.*)|opacity-(.*)|order-(.*)|orphans-(.*)|outline-(.*)|outline-color-(.*)|outline-offset-(.*)|outline-style-(.*)|outline-width-(.*)|overflow-(.*)|overflow-x-(.*)|overflow-y-(.*)|padding-(.*)|padding-bottom-(.*)|padding-left-(.*)|padding-right-(.*)|padding-top-(.*)|page-break-after-(.*)|page-break-before-(.*)|page-break-inside-(.*)|perspective-(.*)|perspective-origin-(.*)|position-(.*)|quotes-(.*)|resize-(.*)|right-(.*)|table-layout-(.*)|tab-size-(.*)|text-align-(.*)|text-align-last-(.*)|text-decoration-(.*)|text-decoration-color-(.*)|text-decoration-line-(.*)|text-decoration-style-(.*)|text-indent-(.*)|text-justify-(.*)|text-overflow-(.*)|text-shadow-(.*)|text-transform-(.*)|top-(.*)|transform-(.*)|transform-origin-(.*)|transform-style-(.*)|transition-(.*)|transition-property-(.*)|transition-duration-(.*)|transition-timing-function-(.*)|transition-delay-(.*)|unicode-bidi-(.*)|vertical-align-(.*)|visibility-(.*)|white-space-(.*)|width-(.*)|word-break-(.*)|word-spacing-(.*)|word-wrap-(.*)|widows-(.*)|z-index-(.*)|(.*))$/,
             sizes: /^(px|pt|em|rem|vw|vh|vmin|vmax)$/,
             prefix: /^(-moz-(.*)|-webkit-(.*)|-o-(.*)|-ms-(.*))$/,
@@ -227,10 +227,14 @@ window.Conf = {
                 for (var i = 0; i < ds.length; i++) {
                     var p = (!_options.return ? ds[i].className.split(' ') : (_options.return).split(' '));
                     if(!_options.return) {
-                        if(typeof ds[i].attributes[options.attr] == "object") {
-                            var va = ds[i].attributes[options.attr].value.replace('{', '').replace('}', '').replace('}', '').replace(/(?:\r\n|\r|\n)/g, ' ').split(' ');
-                            for (var ids in va)
-                                va[ids] != "" ? p.push(va[ids]) : "";
+                        for (var key in ds[i].attributes) {
+                            if (typeof ds[i].attributes[key] == "object") {
+                                if(options.attr.indexOf(ds[i].attributes[key].name) != -1) {
+                                    var va = ds[i].attributes[key].value.replace('{', '').replace('}', '').replace('}', '').replace(/(?:\r\n|\r|\n)/g, ' ').split(' ');
+                                    for (var ids in va)
+                                        va[ids] != "" ? p.push(va[ids]) : "";
+                                }
+                            }
                         }
                     }
                     for (var id in p) {
@@ -252,8 +256,7 @@ window.Conf = {
                             if(!_options.return) {
                                 ds[i].style[(m[0] in options.replace) ? m[0] = options.replace[m[0]]:m[0]] = m[1] + (m[1] % 1 === 0 ? options.size :'');
                             } else {
-                                var key = Object.keys(options.replace).filter(function(key) {return options.replace[key] === options.replace[m[0]]})[0];
-                                _returned.push(key + ': ' + m[1] + (m[1] % 1 === 0 ? options.size :''));
+                                _returned.push(Object.keys(options.replace).filter(function(key) {return options.replace[key] === options.replace[m[0]]})[0] + ': ' + m[1] + (m[1] % 1 === 0 ? options.size :''));
                             }
                         }
                         options = copyObj(_options,'render');
