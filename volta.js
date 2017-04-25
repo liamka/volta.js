d7622923808a4c2e1fa33850b565b9228176c98b
liamka <liamka@me.com>
2017-04-25 15:55:01 +0300

    4.12.14

--- volta.js
+++ volta.js
@@ -1,7 +1,7 @@
 window.Conf = {
     options: {
-        run: false,
         render: {
+            run: false,
             attr: ["volta", "vt"],
             sub_strings: /^(align-content-(.*)|align-items-(.*)|align-self-(.*)|animation-(.*)|animation-delay-(.*)|animation-direction-(.*)|animation-duration-(.*)|animation-fill-mode-(.*)|animation-iteration-count-(.*)|animation-name-(.*)|animation-timing-function-(.*)|animation-play-state-(.*)|background-(.*)|background-attachment-(.*)|background-color-(.*)|background-image-(.*)|background-position-(.*)|background-repeat-(.*)|background-clip-(.*)|background-origin-(.*)|background-size-(.*)|backface-visibility-(.*)|border-(.*)|border-bottom-(.*)|border-bottom-color-(.*)|border-bottom-left-radius-(.*)|border-bottom-right-radius-(.*)|border-bottom-style-(.*)|border-bottom-width-(.*)|border-collapse-(.*)|border-color-(.*)|border-image-(.*)|border-image-outset-(.*)|border-image-repeat-(.*)|border-image-slice-(.*)|border-image-source-(.*)|border-image-width-(.*)|border-left-(.*)|border-left-color-(.*)|border-left-style-(.*)|border-left-width-(.*)|border-radius-(.*)|border-right-(.*)|border-right-color-(.*)|border-right-style-(.*)|border-right-width-(.*)|border-spacing-(.*)|border-style-(.*)|border-top-(.*)|border-top-color-(.*)|border-top-left-radius-(.*)|border-top-right-radius-(.*)|border-top-style-(.*)|border-top-width-(.*)|border-width-(.*)|bottom-(.*)|box-decoration-break-(.*)|box-shadow-(.*)|box-sizing-(.*)|caption-side-(.*)|clear-(.*)|clip-(.*)|color-(.*)|column-count-(.*)|column-fill-(.*)|column-gap-(.*)|column-rule-(.*)|column-rule-color-(.*)|column-rule-style-(.*)|column-rule-width-(.*)|columns-(.*)|column-span-(.*)|column-width-(.*)|content-(.*)|counter-increment-(.*)|counter-reset-(.*)|cursor-(.*)|direction-(.*)|display-(.*)|empty-cells-(.*)|filter-(.*)|flex-(.*)|flex-basis-(.*)|flex-direction-(.*)|flex-flow-(.*)|flex-grow-(.*)|flex-shrink-(.*)|flex-wrap-(.*)|float-(.*)+|css-float-(.*)|font-(.*)|font-family-(.*)|font-size-(.*)|font-style-(.*)|font-variant-(.*)|font-weight-(.*)|font-size-adjust-(.*)|font-stretch-(.*)|hanging-punctuation-(.*)|height-(.*)|hyphens-(.*)|icon-(.*)|image-orientation-(.*)|justify-content-(.*)|left-(.*)|letter-spacing-(.*)|line-height-(.*)|list-style-(.*)|list-style-image-(.*)|list-style-position-(.*)|list-style-type-(.*)|margin-(.*)|margin-bottom-(.*)|margin-left-(.*)|margin-right-(.*)|margin-top-(.*)|max-height-(.*)|max-width-(.*)|min-height-(.*)|min-width-(.*)|nav-down-(.*)|nav-index-(.*)|nav-left-(.*)|nav-right-(.*)|nav-up-(.*)|opacity-(.*)|order-(.*)|orphans-(.*)|outline-(.*)|outline-color-(.*)|outline-offset-(.*)|outline-style-(.*)|outline-width-(.*)|overflow-(.*)|overflow-x-(.*)|overflow-y-(.*)|padding-(.*)|padding-bottom-(.*)|padding-left-(.*)|padding-right-(.*)|padding-top-(.*)|page-break-after-(.*)|page-break-before-(.*)|page-break-inside-(.*)|perspective-(.*)|perspective-origin-(.*)|position-(.*)|quotes-(.*)|resize-(.*)|right-(.*)|table-layout-(.*)|tab-size-(.*)|text-align-(.*)|text-align-last-(.*)|text-decoration-(.*)|text-decoration-color-(.*)|text-decoration-line-(.*)|text-decoration-style-(.*)|text-indent-(.*)|text-justify-(.*)|text-overflow-(.*)|text-shadow-(.*)|text-transform-(.*)|top-(.*)|transform-(.*)|transform-origin-(.*)|transform-style-(.*)|transition-(.*)|transition-property-(.*)|transition-duration-(.*)|transition-timing-function-(.*)|transition-delay-(.*)|unicode-bidi-(.*)|vertical-align-(.*)|visibility-(.*)|white-space-(.*)|width-(.*)|word-break-(.*)|word-spacing-(.*)|word-wrap-(.*)|widows-(.*)|z-index-(.*)|(.*))$/,
             sizes: /^(px|pt|em|rem|vw|vh|vmin|vmax)$/,
@@ -294,6 +294,8 @@ window.Conf = {
          Resize
          */
         Volta.resize = function(options) {
+            if(!window.Conf.options['render'].run)
+                Volta.render({});
             this.options = copyObj(options,'resize');
             function render(obj,options) {
                 for(var index in obj.props.sizeDivs) {
@@ -303,62 +305,13 @@ window.Conf = {
             function handler(options) {
                 var Obj = new Volta();
                 var ds = document.querySelectorAll(options.elements);
-
-
-                console.log(options.elements)
-
-                var el = document.querySelector('section[role=logo]');
-                var style = window.getComputedStyle ? getComputedStyle(el, null).getPropertyValue('font-size') : el.currentStyle.getPropertyValue('font-size');
-
-                console.log(style)
-
-
-                document.onreadystatechange = function(){
-                    for (var i = 0; i < ds.length; i++) {
-                        //console.log(ds[i])
-
-                        //console.log(ds[i])
-
-                        //console.log(112312312412412)
-
-
-
-                    }
-                }
-
-
-
-
-                for (var i = 0; i < ds.length; i++) {
-
-
-
-
-
-
-
-
-
-
-
-
-
+                for (var i = 0; i < ds.length; i++)
                     Obj.props.sizeDivs[i] = {ds:ds[i],sz:parseFloat(document.defaultView.getComputedStyle(ds[i],null).getPropertyValue('font-size'))};
-                }
-
                 render(Obj,options);
                 window.addEventListener('resize', function(){
                     render(Obj,options);
                 }, true);
             }
-
-            console.log(window.Conf.options.run)
-
-            console.log("fdgdfgdf")
-
-
-
-
             handler(this.options);
         }
 
@@ -417,7 +370,7 @@ window.Conf = {
          * Custom functions
          */
         function copyObj(obj,name) {
-            window.Conf.options.run = true; // state
+            window.Conf.options[name].run = true; // state
             if (!obj) obj = {};
             var obj2 = {};
             for (var opt in Conf.options[name]) {
@@ -425,7 +378,6 @@ window.Conf = {
             }
             return obj2;
         }
-
         return Volta;
     })();
 
@@ -434,7 +386,5 @@ if(typeof vlog != 'function'){
     window.vlog = function(mess,options){Volta.vlog(mess,options)};
 }
 document.onreadystatechange = function(){
-    if(!window.Conf.options.run) {
-        Volta.render({});
-    }
+    Volta.render({});
 }
